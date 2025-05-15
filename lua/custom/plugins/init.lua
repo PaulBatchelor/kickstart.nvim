6da75e4993d7529311bc033622a1c0e03ccff5f4
Paul Batchelor <paul@macro.com>
2025-05-15 14:21:26 -0400

    more typescript troubleshooting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,33 +3,54 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    {
-      "pmizio/typescript-tools.nvim",
-      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-      opts = {
-          settings = {
-              -- Disable formatting
-              formatting = {
-                  enable = false
-              },
-              -- Disable auto import organization
-              tsserver = {
-                  formatOnType = false,
-                  organizeImportsOnSave = false
-              }
-          },
-          -- turns off auto-format on save
-          -- on_attach = function(client)
-          --     client.server_capabilities.documentFormattingProvider = false
-          --     client.server_capabilities.documentRangeFormattingProvider = false
-          -- end
-      },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   opts = {
+  --       settings = {
+  --           -- Disable formatting
+  --           formatting = {
+  --               enable = false
+  --           },
+  --           -- Disable auto import organization
+  --           tsserver = {
+  --               formatOnType = false,
+  --               organizeImportsOnSave = false
+  --           }
+  --       },
+  --       -- turns off auto-format on save
+  --       -- on_attach = function(client)
+  --       --     client.server_capabilities.documentFormattingProvider = false
+  --       --     client.server_capabilities.documentRangeFormattingProvider = false
+  --       -- end
+  --   },
+  -- },
+  {
+    'junegunn/fzf',
+    build = './install --all',
+    dependencies = {
+      'junegunn/fzf.vim',
     },
+  },
+  -- {
+  --   'jay-babu/mason-null-ls.nvim',
+  --   event = { 'BufReadPre', 'BufNewFile' },
+  --   dependencies = {
+  --     'williamboman/mason.nvim',
+  --     'nvimtools/none-ls.nvim',
+  --   },
+  --   config = function()
+  --     require 'your.null-ls.config' -- require your null-ls config here (example below)
+  --   end,
+  -- },
+
+  {
+    'neovim/nvim-lspconfig',
+    'jose-elias-alvarez/null-ls.nvim',
+    'nvim-treesitter/nvim-treesitter',
     {
-        'junegunn/fzf',
-        build = './install --all',
-        dependencies = {
-            'junegunn/fzf.vim'
-        },
-    }
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+  },
 }
